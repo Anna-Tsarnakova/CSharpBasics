c5a5b8ec2f8102ba7428cebc550c01b53f6b127f
Anna Tsarnakova <anna.tsarnakova@gmail.com>
2014-11-02 20:49:24 +0200

    Projects
    Intro to programming C#

---
+++ 01.Introduction-to-Programming-Homework/07.PrintMyFirstAndLastName/PrintMyFirstAndLastName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class PrintMyFirstAndLastName
+{
+    static void Main()
+    {
+        Console.WriteLine("My first name is Anna.");
+        Console.WriteLine("My last name is Tsarnakova.");
+    }
+}
+
---
+++ 01.Introduction-to-Programming-Homework/14.PrintCurrentDateTime/PrintCurrentDateTime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+    class PrintCurrentDateTime
+    {
+        static void Main()
+        {
+            Console.Write("Current date and time: ");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+

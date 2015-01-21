922411e668c89134b233cd23894907235427ddf4
anna.tsarnakova@gmail.com <anna.tsarnakova@gmail.com>
2015-01-21 11:46:26 +0200

    Intro to Programming

---
+++ 01. Introduction-to-Programming/06.PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class PrintNumbers
+{
+    static void Main()
+    {
+        int i = 1;
+        Console.WriteLine(i);
+        Console.WriteLine(i + 100);
+        Console.WriteLine(i + 1000);
+     }
+}
